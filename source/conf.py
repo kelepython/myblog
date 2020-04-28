bcdfd92f457964c52ead6bba3784a2b6d7737f42
kelepython <517155992@qq.com>
2020-04-28 17:29:19 +0800

    change config

--- source/conf.py
+++ source/conf.py
@@ -90,3 +90,13 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+_exts = "../exts"
+sys.path.append(os.path.abspath(_exts))
+
+html_js_files = [
+    'js/readmore.js',
+    'js/baidutongji.js',
+]
